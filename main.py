ff91f9c8c4c43c1f5266cd7f1dc4c314275bfc43
BHAVESH GUPTA <35276600+bhavesh-g@users.noreply.github.com>
2024-02-08 00:38:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,6 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')  # Set cors_allowed_origins to '*' for development; restrict in production
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@socketio.on('connect')
-def handle_connect():
-    print('Frontend connected through socket.')
 
 def send_alert_conditionally():
     current_time_seconds = int(time.time())
@@ -26,11 +19,10 @@ def send_alert_conditionally():
         alert_message = f"Random Alert: {random.randint(1, 100)}"
         socketio.emit('receive_alert', {'message': alert_message}, broadcast=True)
 
-def schedule_alert_sender():
-    schedule.every(10).seconds.do(send_alert_conditionally)  # Adjust the interval as needed
 
 if __name__ == '__main__':
-    t = threading.Thread(target=schedule_alert_sender)
-    t.start()
+    
     socketio.run(app, debug=True, port=5000)
     print('stuff')
+    while True:
+        send_alert_conditionally()
